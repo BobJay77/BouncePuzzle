97134f7671e792034922110ac0276da1b8b0a0ae
BuddhaDBread <pranayanda@gmail.com>
2023-10-04 01:28:30 -0400

    Scaling Gameobjects to screen size
    - Add the ScaleToScreenSize script to gameobjects in scene

---
+++ BouceP/Assets/Scripts/Game Functionality/ScaleToScreenSize.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ScaleToScreenSize : MonoBehaviour
+{
+    [SerializeField] private float widthPercentage  = 0.5f; // Percentage of screen width
+    //[SerializeField] float heightPercentage = 0.5f; // Percentage of screen height
+
+    private GameObject startPosition;
+
+    void Start()
+    {
+        startPosition = GameObject.FindGameObjectWithTag("Projectile");
+        
+        
+        // Get the screen width and height
+        float screenWidth = Screen.width;
+        //float screenHeight = Screen.height;
+
+        // Calculate the new scale based on the screen size
+        float newScaleX = screenWidth * widthPercentage / 100f;
+        //float newScaleY = screenHeight * heightPercentage / 100f;
+
+        // Apply the new scale to the GameObject
+        transform.localScale = new Vector3(newScaleX, newScaleX, transform.localScale.z);
+
+        if(startPosition != null)
+        {
+            transform.position = startPosition.transform.position;
+        }
+
+    }
+}
