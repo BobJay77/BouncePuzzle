b12e27ca5ca837b6ddf0006833f8de77cf57ff4c
BuddhaDBread <pranayanda@gmail.com>
2023-10-07 00:07:36 -0400

    Scale to screen size script
    - scales gameobjects in levels

--- BouceP/Assets/Scripts/Game Functionality/ScaleToScreenSize.cs
+++ BouceP/Assets/Scripts/Game Functionality/ScaleToScreenSize.cs
@@ -2,31 +2,32 @@ using UnityEngine;
 
 public class ScaleToScreenSize : MonoBehaviour
 {
-    //[SerializeField] private float widthPercentage  = 0.5f; // Percentage of screen width
-    ////[SerializeField] float heightPercentage = 0.5f; // Percentage of screen height
+    [SerializeField] private float widthPercentage  = 0.5f; // Percentage of screen width
+    [SerializeField] private float heightPercentage = 0.5f; // Percentage of screen height
+    [SerializeField] private float depthScale       = 1f; // Percentage of screen height
 
-    //private GameObject startPosition;
+    private GameObject startPosition;
 
-    //void Start()
-    //{
-    //    startPosition = GameObject.FindGameObjectWithTag("Projectile");
-        
-        
-    //    // Get the screen width and height
-    //    float screenWidth = Screen.width;
-    //    //float screenHeight = Screen.height;
+    void Start()
+    {
+        startPosition = GameObject.FindGameObjectWithTag("Projectile");
 
-    //    // Calculate the new scale based on the screen size
-    //    float newScaleX = screenWidth * widthPercentage / 100f;
-    //    //float newScaleY = screenHeight * heightPercentage / 100f;
 
-    //    // Apply the new scale to the GameObject
-    //    transform.localScale = new Vector3(newScaleX, newScaleX, newScaleX);
+        // Get the screen width and height
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
 
-    //    if(startPosition != null)
-    //    {
-    //        transform.position = startPosition.transform.position;
-    //    }
+        // Calculate the new scale based on the screen size
+        float newScaleX = screenWidth * widthPercentage / 100f;
+        float newScaleY = screenHeight * heightPercentage / 100f;
 
-    //}
+        // Apply the new scale to the GameObject
+        transform.localScale = new Vector3(newScaleX, newScaleY, depthScale);
+
+        if (startPosition != null)
+        {
+            transform.position = startPosition.transform.position;
+        }
+
+    }
 }
